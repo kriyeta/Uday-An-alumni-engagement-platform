0f42b8dbf085457f3d1264b3484ac8a47330b3a9
kamal tiwari <kamaltiwari25@gmail.com>
2016-03-29 01:43:02 +0530

    Update pg_trgm_extension

--- db/migrate/20160327171815_add_pg_trgm_extension_to_db.rb
+++ db/migrate/20160327171815_add_pg_trgm_extension_to_db.rb
@@ -1,5 +1,13 @@
 class AddPgTrgmExtensionToDb < ActiveRecord::Migration
-  def change
-    execute "create extension pg_trgm;"
+  def up
+    execute "CREATE EXTENSION pg_trgm;"
+    execute "CREATE EXTENSION fuzzystrmatch;"
+    execute "CREATE EXTENSION unaccent"
+  end
+
+  def down
+    execute "DROP EXTENSION pg_trgm;"
+    execute "DROP EXTENSION fuzzystrmatch;"
+    execute "DROP EXTENSION unaccent"
   end
 end
