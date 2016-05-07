192aaf157cd700f9748e00dbc2631484bfd06f94
kamal tiwari <kamaltiwari25@gmail.com>
2016-05-07 14:48:56 +0530

    Add events model

---
+++ db/migrate/20160507091802_create_events.rb
@@ -0,0 +1,21 @@
+class CreateEvents < ActiveRecord::Migration
+  def change
+    create_table :events do |t|
+      t.string :title
+      t.text :summary
+      t.string :status
+      t.string :type
+      t.string :target_audiance
+      t.string :organizers
+      t.string :guests
+      t.date :event_date
+      t.time :event_from_time
+      t.time :event_to_time
+      t.text :venue
+      t.string :image_path
+      t.text :details
+
+      t.timestamps null: false
+    end
+  end
+end
