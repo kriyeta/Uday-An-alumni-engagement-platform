f473bbb61034db5efa459e0834e0a5f77d1f43c0
kamal tiwari <kamaltiwari25@gmail.com>
2016-05-08 17:01:22 +0530

    Complete events display helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
   ActionView::Base.default_form_builder = StandardBuilder
+
   def devise_mapping
     Devise.mappings[:user]
   end
@@ -10,5 +11,33 @@ module ApplicationHelper
 
   def resource_class
     devise_mapping.to
+  end
+
+  def display_events(events)
+    html_to_render="<hr>"
+    puts @events.size.to_s.red
+    for event in events
+      html_to_render << "<li class='media'>"
+      if event.image_path.blank?
+        html_to_render << "<a class='pull-left' href='#'>"+image_tag('events/events'+rand(1..2).to_s+'.jpg', :alt => 'Event', :class => 'img-responsive media-object', :style => 'align:center;')+"</a>"
+      else
+        html_to_render << "<a class='pull-left' href='#'>"+image_tag(event.image_path, :alt => 'Event', :class => 'img-responsive', :style => 'align:center;')+"</a>"
+      end
+      html_to_render << "<div class='media-body'>"+"<h4 class='media-heading'>"+event.title.truncate_words(20, omission: ' ')
+      if event.created_at >= Date.today-7
+        html_to_render << "&nbsp; <span class='label label-warning'>New</span>"
+      end
+      html_to_render << "</h4>"
+      html_to_render << "<p>" + event.summary.truncate_words(35, omission: "<a style='text-decoration:none' href='#'> &nbsp;&nbsp;Read More>></a>") +"</p>"
+      html_to_render << "<strong> Event Date: </strong>"+event.event_date.strftime('%d-%b-%Y')
+      if !event.event_from_time.blank?
+        html_to_render << "<strong> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; From: </strong>"+event.event_from_time.strftime('%I:%M%p')
+      end
+      if !event.event_to_time.blank?
+        html_to_render << "<strong> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; To: </strong>"+event.event_to_time.strftime('%I:%M%p')
+      end
+      html_to_render << "</div></li><hr>"
+    end
+    html_to_render.html_safe
   end
 end
