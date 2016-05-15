d02062990768ca3e1151592a2e6571fea725c3cf
kamal tiwari <kamaltiwari25@gmail.com>
2016-05-16 00:54:27 +0530

    Update agenda in home page

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -1,7 +1,8 @@
 class HomePageController < ApplicationController
-  before_action :authenticate_user! , :except => [:home]
-  
+  before_action :authenticate_user!, :except => [:home]
+
   def home
     @events = Event.where('event_date > ?', DateTime.now).order("event_date ASC").limit(3)
+    @agendas = Agenda.all.order("updated_at DESC").limit(3)
   end
 end
